7142e1a167b13d3bae4a252847b87c8dd7d34f42
gibran-toriz <gibran.toriz@gmail.com>
2024-03-05 12:04:30 +0000

    chore: cleaning the project

--- src/auth/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-/**
- * Decorator function that sets the metadata for the roles allowed to access a resource.
- * @param roles The roles allowed to access the resource.
- */
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
